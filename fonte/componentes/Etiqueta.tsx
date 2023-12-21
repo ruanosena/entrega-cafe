ecd62396001519e09d00aa8a021db4b05416c0b3
ruanosena <ruosena@hotmail.com>
2023-12-21 16:05:25 -0300

    animacao do filtro selecionado

--- fonte/componentes/Etiqueta.tsx
+++ fonte/componentes/Etiqueta.tsx
@@ -1,6 +1,9 @@
 import { ReactNode, useEffect, useState } from "react";
 import { Pressable, PressableProps, StyleSheet, Text } from "react-native";
 import { TEMA } from "../estilos/tema";
+import Animated, { useAnimatedStyle, useSharedValue } from "react-native-reanimated";
+
+const PressableAnimado = Animated.createAnimatedComponent(Pressable);
 
 type Props = PressableProps & {
 	aoSelecionar?: (selecionado: boolean) => void;
@@ -17,21 +20,39 @@ export function Etiqueta({
 	...rest
 }: Props) {
 	const [selecionado, defSelecionado] = useState(selecionadoPadrao);
+	const scale = useSharedValue(1);
+
+	const conteinerAnimadoEstilo = useAnimatedStyle(() => ({
+		transform: [{ scale: scale.value }],
+	}));
+
+	function onPressIn() {
+		scale.value = 1.15;
+	}
+	function onPressOut() {
+		scale.value = 1;
+	}
 
 	useEffect(() => {
 		aoSelecionar && aoSelecionar(selecionado);
 	}, [selecionado]);
 
 	return (
-		<Pressable
+		<PressableAnimado
+			onPressIn={onPressIn}
+			onPressOut={onPressOut}
 			onPress={() => {
 				if (deveMudar) defSelecionado(!selecionado);
 			}}
-			style={[estilos.conteiner, selecionado && estilos.conteinerSelecionado]}
+			style={[
+				estilos.conteiner,
+				selecionado && estilos.conteinerSelecionado,
+				conteinerAnimadoEstilo,
+			]}
 			{...rest}
 		>
 			<Text style={[estilos.texto, selecionado && estilos.textoSelecionado]}>{children}</Text>
-		</Pressable>
+		</PressableAnimado>
 	);
 }
 
